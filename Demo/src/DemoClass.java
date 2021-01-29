48a0396fb5e5197577165ec438c0ea3195dfac6c
Admin <Admin@LAPTOP-RQPJ5Q92>
2021-01-29 14:43:06 +0700

    Second

---
+++ Demo/src/DemoClass.java
@@ -0,0 +1,14 @@
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		println("hai");
+	}
+
+	private static void println(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
